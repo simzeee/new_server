5e5cb22047f1ef8eb90c974330ab70eec8e52129
John Sims <jwsims816@gmail.com>
2022-10-31 23:09:38 -0400

    added 2 static websites

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
 // A program that receives and sends messages. Specifically HTTP messages.
 
-const http = require("http");
-const chalk = require("chalk");
+const chalk = require('chalk');
 
-const server = http.createServer();
+const express = require('express');
+const path = require('path');
 
-server.on("request", (request, res) => {
-  // res.writeHead(200, { "Content-Type": "application/json" });
-  if (request.url === "/party") {
-    res.setHeader("Content-Type", "text/html")
-    res.writeHead(200);
-    console.log('what')
+const app = express();
+const port = 8000;
 
-    res.end(`<style>
-    body {
-      background-color: black;
-      color: white;
-    }
-    </style><html>You found the party. &#127865 &#127866	&#127867 &#127849</html>`);
-  } else {
-    res.end("Hello World");
-  }
+app.use('/relax', express.static('relax'));
+app.use('/countdown', express.static('countdown'))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! I am using express.');
 });
 
-// The default port when using HTTP is 80 and HTTPS is 443
-const port = 8000;
-server.listen(port, () => {
+// app.get('/test', (req, res) => {
+//   console.log(__dirname)
+//   // res.sendFile(path.join(__dirname, '/Relaxer_App/index.html'));
+//   // res.render('public/index.html')
+// });
+
+app.listen(port, () => {
   console.log(chalk.green(`listening on port ${port}`));
 });
+
+// const http = require("http");
+// const server = http.createServer();
+
+// server.on("request", (request, res) => {
+//   // res.writeHead(200, { "Content-Type": "application/json" });
+//   if (request.url === "/party") {
+//     res.setHeader("Content-Type", "text/html")
+//     res.writeHead(200);
+//     console.log('what')
+
+//     res.end(`<style>
+//     body {
+//       background-color: black;
+//       color: white;
+//     }
+//     </style><html>You found the party. &#127865 &#127866	&#127867 &#127849</html>`);
+//   } else {
+//     res.end("Hello World");
+//   }
+// });
+
+// // The default port when using HTTP is 80 and HTTPS is 443
+// server.listen(port, () => {
+//   console.log(chalk.green(`listening on port ${port}`));
+// });
